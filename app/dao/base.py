d465ae713247be4575c14602e45815c983a99414
nikolai <ngbaranov@gmail.com>
2024-11-07 16:52:02 +0300

    dao

---
+++ app/dao/base.py
@@ -0,0 +1,18 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+class BaseDAO:
+    model = None
+    @classmethod
+    async def get_all_users(cls, session: AsyncSession):
+        # Создаем запрос для выборки всех пользователей
+        query = select(cls.model)
+
+        # Выполняем запрос и получаем результат
+        result = await session.execute(query)
+
+        # Извлекаем записи как объекты модели
+        records = result.scalars().all()
+
+        # Возвращаем список всех пользователей
+        return records
--- app/routers/input_user.py
+++ app/routers/input_user.py
@@ -3,9 +3,8 @@ from fastapi.templating import Jinja2Templates
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql.functions import session_user
-
+from app.dao.dao import UsersDAO
 from app.models.models import User
-from app.database.db import connection
 from app.database.db_depends import get_db
 
 from typing import Annotated
@@ -13,12 +12,9 @@ from typing import Annotated
 
 router = APIRouter(prefix="/input_user", tags=["input_user"])
 templates = Jinja2Templates(directory="app/templates")
-@connection
 @router.get("/")
 async def get_input_user(request: Request, db: Annotated[AsyncSession, Depends(get_db)]):
-    query = select(User)
-    result = await db.execute(query)
-    users = result.scalars().all()
+    users = await UsersDAO.get_all_users(db)
     return templates.TemplateResponse("input_user.html", {"request": request, "users": users})
 
 
